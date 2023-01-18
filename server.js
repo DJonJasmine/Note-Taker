23fb80b91181c10230875d00c51587c428b2f2c7
unknown <harrisondjon1994@gmail.com>
2023-01-18 13:53:09 -0800

    updated db.json file and server.js file

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 // importing packages
+const fs = require('fs');
 const express = require('express');
 const path = require('path');
 // initializing app and port
 const app = express();
 const PORT = process.env.PORT || 3001;
 // json file
-const dbData = require('./Develop/db/db.json');
+const dbData = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -13,11 +14,17 @@ app.use(express.static('public'));
 
 app.use(express.json());
 
+app.get('/api/notes', (req, res) => {
+    res.json(dbData.slice(1));
+});
 
-
+// get index.html on start
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 // get notes and return them to notes.html
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/assets/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 // get all files returned to index.html
 app.get('*', (req, res) => {
@@ -25,6 +32,30 @@ app.get('*', (req, res) => {
 });
 // get api and returning json in db.json
 app.get('/api', (req, res) => res.json(dbData));
+
+//create notes
+function createNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    );
+    return note;
+}
+
+// new note saved to request body, added to db.json file then returned as a new note
+app.post('/api/notes', (req, res) => {
+    const newNote = createNote(req.body, dbData);
+    res.json(newNote);
+});
+
+//delete note
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, dbData);
+    res.json(true);
+});
 
 // start server on the port
 app.listen(PORT, () =>
